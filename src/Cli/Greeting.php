05c51eb87ecfedb6b4dfb231bedbbcbef998a18a
AlexeyKrasnopevtsev <fktrctq321@gmail.com>
2026-03-17 15:47:22 +0700

    Add all SonarCloud

--- src/Cli/Greeting.php
+++ src/Cli/Greeting.php
@@ -6,7 +6,6 @@ class Greeting
 {
     public static function hello(): string
     {
-        
         \cli\line('Welcome to the Brain Game!');
         $name = \cli\prompt('May I have your name?');
         \cli\line("Hello, %s!", $name);
